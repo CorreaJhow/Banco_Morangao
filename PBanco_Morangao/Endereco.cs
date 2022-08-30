718835b10f34b0980296a7f19929d8afb1b546c4
Jhonatas Rodrigues Correa <jhonatasrcorrea@gmail.com>
2022-08-30 09:47:11 -0300

    Adicionar arquivos de projeto.

---
+++ PBanco_Morangao/Endereco.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBanco_Morangao
+{
+    internal class Endereco
+    {
+        String Bairro;
+        String Rua;
+        String Numero;
+        String Cidade;
+        String Cep;
+
+    }
+}
